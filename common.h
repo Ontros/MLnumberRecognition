3264559c3b3a2a9e9597007f0d868496fcaa158c
Ontros <ontro512@gmail.com>
2022-05-19 17:49:06 +0200

    First commit

---
+++ common.h
@@ -0,0 +1,32 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+/*=======STANDARD TEMPLATE LIBRARY=======*/
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <memory>
+#include <malloc.h>
+#include <ctime>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <map>
+// #pragma comment(lib, "winmm.lib")
+// #pragma comment(lib, "Winmm.lib")
+// #include <Windows.h>
+// #include <mmsystem.h>
+// #include <SFML/Audio.hpp>
+#include <sstream>
+#include <csignal>
+#include <unordered_map>
+#include "./Dependencies/include/GLM/glm.hpp"
+#include "./Dependencies/include/GLM/gtc/matrix_transform.hpp"
+#include "./Dependencies/include/GLM/gtc/type_ptr.hpp"
+#include "./Dependencies/include/GLM/gtx/rotate_vector.hpp"
+#include "./Dependencies/include/GLM/gtx/vector_angle.hpp"
+#include "./Dependencies/include/GLM/gtx/intersect.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "./Dependencies/include/stb_image.h"
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "./common.h"
+// C:/MinGW/bin/g++ -g main.cpp -o Builds/Win_Build/engine -lmingw32 -lopengl32 -lglew32 -lglew32mx -lglu32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello world!";
+
+    getchar();
+    return 0;
+}
